5d12db7bd18b10c7abfab92422565736420bccb9
omidiora <omidioraemmanuel@gmail.com>
2021-03-10 20:57:59 +0100

    fix

--- TestAssesment/settings.py
+++ TestAssesment/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'api',
+    
     'rest_framework',
     
 ]
