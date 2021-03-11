8dc889ed9566425e87b5efcfd3870ec000576962
omidiora <omidioraemmanuel@gmail.com>
2021-03-11 16:40:34 +0100

    Database

--- api/models.py
+++ api/models.py
@@ -6,12 +6,12 @@ from django.db import models
 
 class personalInfo(models.Model):
     firstname = models.CharField(max_length=50 )
-    lastname = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=100 ,)
-    contact = models.CharField( max_length=100 ,blank=True )
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    contact = models.CharField( max_length=100)
     company = models.CharField(max_length=100 , blank=True)
     interested =models.CharField(max_length=100,    blank=True )
-    budget=models.CharField(max_length=100, blank=True ,)
+    budget=models.CharField(max_length=100, blank=True)
     images= models.ImageField(upload_to='images' , blank=True  ,null=True)
     about=models.TextField(blank=True)
 
