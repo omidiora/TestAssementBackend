63ede8a5ca807c8d89f0dbe9bd4fb189995c37b4
omidiora <omidioraemmanuel@gmail.com>
2021-03-11 16:04:19 +0100

    Heroky setup

--- api/models.py
+++ api/models.py
@@ -7,11 +7,11 @@ from django.db import models
 class personalInfo(models.Model):
     firstname = models.CharField(max_length=50 )
     lastname = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=100)
-    contact = models.CharField( max_length=100 , blank=True )
+    email = models.EmailField(max_length=100 ,)
+    contact = models.CharField( max_length=100 ,blank=True )
     company = models.CharField(max_length=100 , blank=True)
     interested =models.CharField(max_length=100,    blank=True )
-    budget=models.CharField(max_length=100, blank=True)
+    budget=models.CharField(max_length=100, blank=True ,)
     images= models.ImageField(upload_to='images' , blank=True  ,null=True)
     about=models.TextField(blank=True)
 
